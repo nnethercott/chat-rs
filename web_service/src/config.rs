ed18d78cbc81e366c1af18e1ea9048195f162ca9
nnethercott <nathaniel@deepomatic.com>
2025-04-27 17:56:16 +0200

    axum service working

---
+++ web_service/src/config.rs
@@ -0,0 +1,85 @@
+use std::{
+    env::{self},
+    path::Path,
+};
+
+use config::Config;
+use serde::Deserialize;
+
+type RedisConfig = ServerConfig;
+type GRPCConfig = ServerConfig;
+
+#[derive(Deserialize, Debug)]
+pub struct Settings {
+    #[serde(alias = "web")]
+    pub server: ServerConfig,
+    pub redis: RedisConfig,
+    pub grpc: GRPCConfig,
+}
+
+impl Settings {
+    pub fn new() -> Self {
+        todo!();
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: String,
+}
+
+impl ServerConfig {
+    pub fn addr(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
+pub enum Env {
+    Local,
+    Production,
+}
+
+impl ToString for Env {
+    fn to_string(&self) -> String {
+        match self {
+            Env::Local => "local".into(),
+            Env::Production => "production".into(),
+        }
+    }
+}
+
+impl TryFrom<String> for Env {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match &value[..] {
+            "local" => Ok(Env::Local),
+            "production" => Ok(Env::Production),
+            _ => Err("value must be 'local' or 'production'".to_string()),
+        }
+    }
+}
+
+pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let environ: Env = env::var("AXUM_SERVER_APP_ENV")
+        .unwrap_or("local".into())
+        .try_into()
+        .expect("failed TryInto<Env>");
+
+    let config_file = match env::var("CONFIG_FILE") {
+        Ok(f) => Path::new(&f).to_path_buf(),
+        Err(_) => env::current_dir().unwrap().join(format!(
+            "{}/config/{}.yaml",
+            env!("CARGO_CRATE_NAME"),
+            environ.to_string()
+        )),
+    };
+
+    let c = Config::builder()
+        .add_source(config::File::from(config_file))
+        .add_source(config::Environment::default().separator("__"))
+        .build()?;
+
+    c.try_deserialize()
+}
