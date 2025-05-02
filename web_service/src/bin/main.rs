93c8449ef9fbb207df4b8e110e5059f7873584cd
nnethercott <nathaniel@deepomatic.com>
2025-05-02 08:57:13 +0200

    ignore nested spans in tracing

--- web_service/src/bin/main.rs
+++ web_service/src/bin/main.rs
@@ -6,8 +6,14 @@ async fn main() -> anyhow::Result<()> {
     let config = get_config().expect("failed to build config");
     // let log_level = config.log_level.clone().as_str();
 
+    // more options from docs  where `with_span_list` indicated
     tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().json().with_level(true))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_level(true)
+                .with_span_list(false), // noise
+        )
         .with(EnvFilter::try_from_default_env().unwrap_or("info".into()))
         .init();
 
